d0b30ef1636f97584ab9b3cd8f6da5337c79a9e8
James <jamesgayht@gmail.com>
2022-08-26 01:54:50 +0800

    upd lc14

--- src/main/java/LeetcodePrac/Main.java
+++ src/main/java/LeetcodePrac/Main.java
@@ -4,6 +4,20 @@ public class Main {
     
     public static void main(String[] args) {
 
-        System.out.println(Leetcode13.romanToInt("MMCDXXV"));
+        String[] strs = {"flower", "flowas", "flo"};
+        String[] strs1 = {"dog","racecar","car"};
+        String[] strs2 = {"aa", "aa", "aa"};
+        String[] strs3 = {"apple", "app", "apple"};
+        String[] strs4 = {"a", "ab"};
+        String[] strs5 = {"flower", "flower", "flower", "flower", "flower"};
+
+
+
+        System.out.println(Leetcode14.longestCommonPrefix(strs));
+        System.out.println(Leetcode14.longestCommonPrefix(strs1));
+        System.out.println(Leetcode14.longestCommonPrefix(strs2));
+        System.out.println(Leetcode14.longestCommonPrefix(strs3));
+        System.out.println(Leetcode14.longestCommonPrefix(strs4));
+        System.out.println(Leetcode14.longestCommonPrefix(strs5));
     }
 }
