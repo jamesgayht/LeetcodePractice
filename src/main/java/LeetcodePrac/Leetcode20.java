b091c1ada1f3512fa46af43893bdec9af27800dd
James <jamesgayht@gmail.com>
2022-08-26 15:17:15 +0800

    upd lc20v2

--- src/main/java/LeetcodePrac/Leetcode20.java
+++ src/main/java/LeetcodePrac/Leetcode20.java
@@ -4,6 +4,38 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Leetcode20 {
+
+    public static boolean isValid2(String s) {
+        boolean result = false;
+        char[] charArray = s.toCharArray(); 
+
+        HashMap<Character, Character> hashMap = new HashMap<Character, Character>();
+        hashMap.put('}', '{');
+        hashMap.put(')', '(');
+        hashMap.put(']', '[');
+        LinkedList<Character> linkedList = new LinkedList<>();
+
+        for(char c : charArray) {
+            if(hashMap.containsKey(c)) { // checking for closing brakcets
+                if(linkedList.size() != 0 && linkedList.get(linkedList.size()-1) == hashMap.get(c)) {
+                    linkedList.remove(linkedList.size()-1); 
+                } else {
+                    return result; 
+                }
+            } else { //checking for open brackets
+                linkedList.add(c);
+            }
+        }
+        if (linkedList.isEmpty()) {
+            result = true;
+            return result;
+        } else {
+            return result;
+        }
+    }
+
+
+
     public static boolean isValid(String s) {
 
         boolean result = false;
--- src/main/java/LeetcodePrac/Main.java
+++ src/main/java/LeetcodePrac/Main.java
@@ -1,12 +1,12 @@
 package LeetcodePrac;
 
-import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(Leetcode20.isValid2("{}{{}}}"));
+        System.out.println(Leetcode20.isValid2("{}{{}}"));
 
     }
 }
